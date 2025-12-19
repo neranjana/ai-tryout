79509c7cfb778b2af15643eadbf2c34d50bd1b89
neranjana <neranjana@gmail.com>
2025-12-19 11:03:36 +1100

    Add comments

--- 01_talk_to_gemini/talk_to_gemini.py
+++ 01_talk_to_gemini/talk_to_gemini.py
@@ -1,7 +1,11 @@
+# Please make sure to install the required packages before running this code:
+# pip install -r requirements.txt in windows or pip3 install -r requirements.txt on Mac
+
 import google.genai as genai # Import the Google Generative AI library
 from dotenv import load_dotenv # Import load_dotenv to manage environment variables
 
-
+# Please make sure to set your Gemini API KEY in a .env file in the same folder, for this code to work.
+# You can copy the .env_sample file and rename it to .env, then add your API key there.
 
 def main(): # Main function to interact with Gemini model
 
@@ -10,7 +14,7 @@ def main(): # Main function to interact with Gemini model
 
     while True: 
         user_input = input("Say something to Gemini (or 'quit' to exit): ") # Get user input
-        if user_input.lower() != "quit": #
+        if user_input.lower() != "quit": # Check if the user does not want to quit
             try:
                 response = client.models.generate_content(
                     model="gemini-2.5-flash", contents=user_input
